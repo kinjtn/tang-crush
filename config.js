86ffc97126edc335a54c947d9598bc1daae83602
kinjtn <85757216+kinjtn@users.noreply.github.com>
2021-06-11 23:48:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'Bống à!',
+    introDesc: `Em muốn biết ai là người con người xinh đẹp nhất thế gian này không`,
+    btnIntro: 'Có chứ',
+    title: 'Thì đó chính là ny anh chứ còn gì nứa 😙',
+    desc: 'Thế có muốn biết ny anh là ai không ',
+    btnYes: 'Có anh iu à <33',
+    btnNo: 'Khồngggg :3',
+    question:'Trên thế giới hơn 7 tỉ người mà trong mắt anh lại chỉ có mình em thôi đó <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 2r call nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
